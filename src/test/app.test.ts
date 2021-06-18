27741ab17a7950bbfb9ecc270731c862ba5ef10f
Katja <kaloh@icloud.com>
2021-06-18 12:04:29 +0200

    add expectations

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,21 +1,27 @@
 import {getStarwarsPeople, getStarwarsPeople2} from "../main/app";
 
 test("can call function", (done) => {
+    let listOfFemalePerson:string[] = new Array()
     getStarwarsPeople((persons) => {
         for (const person of persons) {
             if (person.gender === "female") {
-                console.log(person.name)
+                listOfFemalePerson.push(person.name)
             }
         }
         done()
+        expect(listOfFemalePerson).toContain('Leia Organa')
+        expect(listOfFemalePerson).toContain('Beru Whitesun lars')
     });
 })
 
 test("lists all female Starwars Persons", async ()=> {
+    let listOfFemalePerson:string[] = new Array()
     const persons = await getStarwarsPeople2()
     for(const person of persons){
         if(person.gender === "female"){
-            console.log(person.name)
+            listOfFemalePerson.push(person.name)
         }
     }
+    expect(listOfFemalePerson).toContain("Leia Organa")
+    expect(listOfFemalePerson).toContain("Beru Whitesun lars")
 })
