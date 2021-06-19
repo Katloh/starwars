44f598a9789f9b80809023b36224d367962bc88d
Katja <kaloh@icloud.com>
2021-06-19 21:32:36 +0200

    support paginated responses

--- src/main/app.ts
+++ src/main/app.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const url = "https://swapi.dev/api/people"
+let url = "https://swapi.dev/api/people"
 
 export function getStarwarsPeople(callback: any) {
     console.log("getting");
@@ -13,6 +13,45 @@ export function getStarwarsPeople(callback: any) {
 export async function getStarwarsPeople2() {
     return (await axios.get(url)).data.results
 }
+
+type StarWarsPeoplePage = {
+    next: string
+    results: Character[]
+}
+
+export type Character = {
+    name: string
+    gender: string
+}
+
+export async function getAllStarWarsPeople() {
+    let listOfStarWarsPeopleOfPage: Character[] = new Array()
+    let listOfAllStarWarsPeople: Character[] = new Array()
+
+    do{
+        let data: StarWarsPeoplePage = (await axios.get<StarWarsPeoplePage>(url)).data
+        listOfStarWarsPeopleOfPage = data.results
+        listOfAllStarWarsPeople.push(...listOfStarWarsPeopleOfPage)
+        url = data.next
+    } while (url !== null)
+
+    return listOfAllStarWarsPeople
+}
+
+export async function getAllFemaleStarWarsPeople():Promise<Character[]> {
+    let listOfAllFemaleStarWarsPeople: Character[] = new Array()
+    let listOfAllStarWarsPeople:Character[] = await getAllStarWarsPeople()
+    for(const person of listOfAllStarWarsPeople){
+        if(person.gender === "female"){
+            listOfAllFemaleStarWarsPeople.push(person)
+        }
+    }
+    return listOfAllFemaleStarWarsPeople
+}
+
+
+
+
 
 
 
